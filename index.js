dc7be04e0aaf81f6484fd0efafc11c3013412b1c
masum691 <masumhu691@gmail.com>
2021-11-10 15:08:58 +0600

    added mongo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('dotenv').config()
+const { MongoClient } = require('mongodb');
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
@@ -7,6 +8,11 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.icwz7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
