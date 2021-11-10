7558899870dbbc3a454af0f414f11fc4bfb408c7
masum691 <masumhu691@gmail.com>
2021-11-10 14:57:21 +0600

    setup cors

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
