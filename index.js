89bb89ab9a69fc2e8b22ab8c26a7b34c2c439f44
masum691 <masumhu691@gmail.com>
2021-11-10 15:24:06 +0600

    connected with client with server with database

--- index.js
+++ index.js
@@ -12,7 +12,30 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.icwz7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri)
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("baby_care");
+      const exploreCollection = database.collection("explore");
+      // explore get
+      app.get('/explore', async (req, res) => {
+          const cursor = exploreCollection.find({});
+          const explore = await cursor.toArray();
+          res.send(explore)
+      })
+
+      // explore post 
+      app.post('/explore', async(req, res) => {
+          const explo = req.body;
+          const result = await exploreCollection.insertOne(explo)
+          res.json(result)
+      })
+
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
